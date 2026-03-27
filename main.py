c4701bf6eb43943276e74d31b43795d3fb2a776f
irinan0 <norinsky.irina@gmail.com>
2026-03-27 11:41:40 +0300

    initial menu

---
+++ main.py
@@ -0,0 +1,7 @@
+while True:
+    print("\n[1] System Info\n[2] Log Checker\n[3] Task List\n[0] Exit")
+    choice = input("Select an option: ")
+    if choice == '1': print("System Info selected")
+    elif choice == '2': print("Log Checker selected")
+    elif choice == '3': print("Task List selected")
+    elif choice == '0': break
