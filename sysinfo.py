468b0bb9f38761de5711298ffe5cd7c528247161
IrinaN <norinsky.irina@gmail.com>
2026-03-27 10:55:06 +0200

    Add system information display function

--- sysinfo.py
+++ sysinfo.py
@@ -1,1 +1,7 @@
+import socket
+import getpass
 
+def show_sysinfo():
+    print(f"Hostname: {socket.gethostname()}")
+    print(f"User: {getpass.getuser()}")
+    print(f"Current Directory: {os.getcwd()}")
